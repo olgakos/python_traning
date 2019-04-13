d86ba46e1998114d60ba58fd0a82f9af91343a89
Olga Kos <ok2304@yandex.ru>
2019-04-13 16:35:57 +0300

    2019-04-13 16-30 Лекция 1.15. "Передача параметров в методы". Вроде бы успешно. Замечания: 1) после прохождения теста FireFox сам закрывается, кажется раньше такого не было. 2) "Дубль" теста, с пустыми параметрами как в примере в лекции, из кода удалила. А то тест проходит с путыми значениями. Но все равно проходит.

--- test_add_group.py
+++ test_add_group.py
@@ -10,9 +10,9 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="1234", header="qwert", footer="zxcvb")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -24,31 +24,31 @@ class TestAddGroup(unittest.TestCase):
         # return to groups page
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
-        # init group creation
+    def create_group(self, wd, name, header, footer):
+                # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("1234")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("qwert")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("zxcvb")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # Submit group creation
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
