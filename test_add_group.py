1adf7d86f948c0d3519890fae6941eee2fe4d881
Olga Kos <ok2304@yandex.ru>
2019-04-14 22:00:03 +0300

    2019-04-14 22-00 лекц.2_02. "Фикстура". Временный комит. "удалены wd", больше не передаются в качестве параметра"

--- test_add_group.py
+++ test_add_group.py
@@ -8,29 +8,28 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
+    def open_home_page(self):
         wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="1234", header="qwert", footer="zxcvb"))
-        self.logout(wd)
+        # open homepage
+        wd.get("http://localhost/addressbook/addressbook/")
 
-    # it's rename double + noname + no header... = new test?
-    def test_add_empty_group(self):
+    def login(self, username, password):
         wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
 
-    def return_to_groups_page(self, wd):
-        # return to groups page
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        self.open_groups_page(wd)
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -45,23 +44,30 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page(wd)
+        self.return_to_groups_page()
 
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+    def return_to_groups_page(self):
+        wd = self.wd
+        # return to groups page
+        wd.find_element_by_link_text("group page").click()
 
-    def open_home_page(self, wd):
-        # open homepage
-        wd.get("http://localhost/addressbook/addressbook/")
+    def logout(self):
+        wd = self.wd
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_group(self):
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="1234", header="qwert", footer="zxcvb"))
+        self.logout()
+
+    # it's rename double + noname + no header... = new test?
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=""))
+        self.logout()
 
     def tearDown(self):
         self.wd.quit()
