3e9c3284e7fbedefc23b4c11f899b0e114ca2e8f
Olga Kos <ok2304@yandex.ru>
2019-04-14 16:23:49 +0300

    2019-04-14 16-23 задание №3. Тест "добавить новый контакт" размечен комментариями. Запускается из среды.

--- test_add_contact3.py
+++ test_add_contact3.py
@@ -11,14 +11,20 @@ class TestAddContact3(unittest.TestCase):
 
     def test_add_contact3(self):
         wd = self.wd
+        #open home page
         wd.get("http://localhost/addressbook/addressbook/")
+        #login
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
+        #password
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
+        #enter
         wd.find_element_by_xpath("//input[@value='Login']").click()
         wd.find_element_by_id("content").click()
+        #init new contact
         wd.find_element_by_link_text("add new").click()
+        #fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Tom")
@@ -28,15 +34,18 @@ class TestAddContact3(unittest.TestCase):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
         wd.find_element_by_name("home").send_keys("111-11-11")
+        #submit contact creation
         wd.find_element_by_xpath("//input[21]").click()
+        #return contacts pages
         wd.find_element_by_id("content").click()
+        #logout
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
