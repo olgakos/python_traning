3fe4f76de6b2276f7d0f81a2bcd94d48fd8cf81d
Olga Kos <ok2304@yandex.ru>
2019-04-15 19:05:02 +0300

    2019-04-15 19-00 задание №3 - "Добавить новый контакт" См. файлы: 1) test_add_contact3.py 2) contact.py

---
+++ contact.py
@@ -0,0 +1,7 @@
+
+class Contact():
+
+    def __init__(self, first_name, second_name, home_phone):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.home_phone = home_phone
--- test_add_group.py
+++ test_add_group.py
@@ -1,76 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
-
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self):
-        wd = self.wd
-        # open homepage
-        wd.get("http://localhost/addressbook/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+from application import Application
 
 
-    def return_to_groups_page(self):
-        wd = self.wd
-        # return to groups page
-        wd.find_element_by_link_text("group page").click()
+#это значок инициализатора фикстуры
+@pytest.fixture
+def app(request):
+    #инициализация создания фикстуры
+    fixture = Application()
+    #указание как фикстура д.б. разрушена
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def logout(self):
-        wd = self.wd
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="1234", header="qwert", footer="zxcvb"))
-        self.logout()
+#тестовый метод прнимающий в качестве параметра фикстуру и вызывающий в ней вспомогательные методы
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="1234", header="qwert", footer="zxcvb"))
+    app.logout()
 
     # it's rename double + noname + no header... = new test?
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
