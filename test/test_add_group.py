ae8c4584d7469e59a46332049916459bc3c36a73
Olga Kos <ok2304@yandex.ru>
2019-04-18 21:02:49 +0300

    2019-04-18 21-00 Лекц.2_04 - Пробую запустить тест delete_first_group - Тест начинает выполняться, залогиниватеся, попадает на главную страницу. - Но на вкладку groups (на которой он должен нажимать чекбокс и кнопку) перейти не может и падает. - Почему?

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-#это значок инициализатора фикстуры
-@pytest.fixture
-def app(request):
-    #инициализация создания фикстуры
-    fixture = Application()
-    #указание как фикстура д.б. разрушена
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 #тестовый метод принимающий в качестве параметра фикстуру и вызывающий в ней вспомогательные методы
 #указываем, в каком пакете теперь искать про ЛОГИН и ЛОГАУТ
