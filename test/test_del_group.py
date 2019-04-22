7febeb313733df8655f36fc1cb3c170803cd0fbd
Olga Kos <ok2304@yandex.ru>
2019-04-22 17:45:20 +0300

    2019-04-22 17.40 Лекц. 2_04 1) Исправлено:  fixture\group - wd.find_element_by_name("selected[]").click() 2) Испаравлено: test/test_del_group - app.group.delete_first_group() - Тест delete_first_group - запускается.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,5 +3,5 @@
 
 def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.delete.first_group()
+    app.group.delete_first_group()
     app.session.logout()
