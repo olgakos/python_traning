7a574c78d73176046b6eb2a1e6b6f127c4df374f
Olga Kos <ok2304@yandex.ru>
2019-04-24 19:43:36 +0300

    2019-04-24 19.23 Лекц. 3_02 Оптимизация ввода данных в формы (fail) (test_modify_group)

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 class GroupHelper:
 
     def __init__(self, app):
@@ -9,55 +8,72 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def change_field_value(self, field_name, text):
+       wd = self.app.wd
+       if text is not None:
+         wd.find_element_by_name(field_name).click()
+         wd.find_element_by_name(field_name).clear()
+         wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
     def create_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # Submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
     def delete_first_group(self):
-        #две строки ниже можно скопировать из пхожих сценариев выше - "открыть стр. с группами"
+        #стандартно переходим на нужную страницу "открыть стр. с группами"
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        #submit deletion (выполнить клик по элементу кнопка) delete и кликнуть по нему"
+        wd.find_element_by_name("delete").click()
+        #затем обращение к методу "return_to_groups_page" чтобы перейти на стр групп
+        self.return_to_groups_page()
+
+    def select_first_group(self):
+        # select first group (выполнить клик по элементу/чекбоксу)
+        wd.find_element_by_name("selected[]").click()
+
+    #это "вспомогательный метод" edit_first_group
+    def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
         #select first group
         wd.find_element_by_name("selected[]").click()
-        #submit deletion (нажать кнопку, т.е. сделать удаление)= "найти нас стр. элемент по имени delete и кликнуть по нему"
-        wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
-
-    def edit_first_group(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        #secect first group
-        wd.find_element_by_name("selected[]").click()
         #submit edition
         wd.find_element_by_name("edit").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group edition
+        # fill group form (это блок кода, заполняющий ФОРМУ из трех полей name+header+footer)
+        #-этот блок можно первратить во вспомю.метод для повторн. использования
+        #-refactor-ectract-metod (+"group")=
+        self.fill_group_form(group)
+        # submit group edition/update
         #wd.find_element_by_name("update").click()
         wd.find_element_by_xpath("// input[@ name='update']").click()
+        self.return_to_groups_page()
+
+    def modify_first_group(self, new_group_date):
+        #Открыть страницу с группами
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        #open modification form
+        wd.find_element_by_name("edit").click()
+        #fill group form
+        self.fill_group_form(new_group_date)
+        #submit modification
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
--- model/group.py
+++ model/group.py
@@ -2,7 +2,7 @@
 #это класс Group из задания №3, теперь в директории model
 class Group():
 
-    def __init__(self, name, header, footer):
+    def __init__(self, name="None", header="None", footer="None"):
         self.name = name
         self.header = header
         self.footer = footer
