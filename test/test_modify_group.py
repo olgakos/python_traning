b9a3487d15f6a54c7522e402684234789150ec0a
Olga Kos <ok2304@yandex.ru>
2019-04-27 12:29:40 +0300

    2019-04-27 12-27 Задание №8 fix Проверка валидности фикстуры и проверка состояния сессии

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,10 +5,10 @@ def test_modify_group_name(app):
     #Unit3_05
     if app.group.count() == 0:
        app.group.create(Group(name="test3"))
-    app.group.modyfy_first_group(Group(name="New group"))
+    app.group.modify_first_group(Group(name="New group"))
 
 def test_modify_group_header(app):
     # Unit3_05
    if app.group.count() == 0:
       app.group.create(Group(name="test3"))
-    app.group.modyfy_first_group(Group(header="New header"))
+   app.group.modify_first_group(Group(header="New header"))
